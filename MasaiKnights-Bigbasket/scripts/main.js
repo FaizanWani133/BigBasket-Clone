f71b94c387f7d3b076ad5a6ef538a813de02be68
FaizanWani133 <97751853+FaizanWani133@users.noreply.github.com>
2022-09-07 12:12:11 +0530

    Add files via upload

--- MasaiKnights-Bigbasket/scripts/main.js
+++ MasaiKnights-Bigbasket/scripts/main.js
@@ -1,17 +1,25 @@
-document.querySelector(".topOffer > img:last-child").addEventListener("click", ()=>{
-    localStorage.setItem("showItemsPriceLessThen30", true)
-    window.location.href = "search.html"
-})
+document
+  .querySelector('.topOffer > img:last-child')
+  .addEventListener('click', () => {
+    localStorage.setItem('showItemsPriceLessThen30', true);
+    window.location.href = '../search.html';
+  });
 
-document.querySelector(".topOffer > img:first-child").addEventListener("click", ()=>{
-    localStorage.setItem("DealsOfTheWeekImg1", true)
-    window.location.href = "search.html"
-})
-document.querySelector(".topOffer > img:nth-child(2)").addEventListener("click", ()=>{
-    localStorage.setItem("BigPackBiggerDiscount", true)
-    window.location.href = "search.html"
-})
-document.querySelector(".topOffer > img:nth-child(3)").addEventListener("click", ()=>{
-    localStorage.setItem("CombosYouCantMiss", true)
-    window.location.href = "search.html"
-})
+document
+  .querySelector('.topOffer > img:first-child')
+  .addEventListener('click', () => {
+    localStorage.setItem('DealsOfTheWeekImg1', true);
+    window.location.href = '../search.html';
+  });
+document
+  .querySelector('.topOffer > img:nth-child(2)')
+  .addEventListener('click', () => {
+    localStorage.setItem('BigPackBiggerDiscount', true);
+    window.location.href = '../search.html';
+  });
+document
+  .querySelector('.topOffer > img:nth-child(3)')
+  .addEventListener('click', () => {
+    localStorage.setItem('CombosYouCantMiss', true);
+    window.location.href = '../search.html';
+  });
